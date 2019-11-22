f8e8a5d42d69339d71ce215cc55d6bee91e9b28c
W.Crama <33249073+wcrama@users.noreply.github.com>
2019-11-22 11:51:14 +0100

    Update install_selenium.sh

--- install_selenium.sh
+++ install_selenium.sh
@@ -27,7 +27,7 @@ read -p "Are you sure you want to continue? <y/N> " prompt
 if [[ $prompt == "y" || $prompt == "Y" || $prompt == "yes" || $prompt == "YES"|| $prompt == "Yes" ]]
 then
   echo "Become SuperUser"
-  sudo su-
+  sudo su -
   echo "Creating folder and move to it..."
   cd /Desktop
   mkdir selenium
